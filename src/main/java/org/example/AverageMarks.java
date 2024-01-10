767f7b09750cec367be8b694b62ce00f958eae08
Geeth-Rath <geeth4862@gmail.com>
2024-01-10 23:22:39 +0530

    completed

--- src/main/java/org/example/AverageMarks.java
+++ src/main/java/org/example/AverageMarks.java
@@ -1,2 +1,31 @@
-package org.example;public class AverageMarks {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AverageMarks {
+
+
+    public void getStudentAverage(String [] stuarray){
+        ArrayList <String> al = new ArrayList();
+        for(String s : stuarray){
+            int sum = 0;
+            String sm [] = s.split("\\s");
+            String name = sm[0] + " " +sm[1];
+
+            for (int i = 2; i <= sm.length-1; i++) {
+                sum = (Integer.valueOf(sm[i]) + sum);
+//
+            }
+            int avgSum = sum / (sm.length-2);
+            if (avgSum < 10){
+                al.add(name+" - " + avgSum);
+            }
+        }
+        for (String item: al){
+            System.out.println(item);
+        }
+
+
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,35 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ *
+ Imagine you are a teacher. At the end of the school year, you need to calculate the average marks of your students.
+ The marks of the students are passed as an array of strings. Each student's detalls are provided in a string in the following format:
+ <student><mark 1><mark 2><mark N>
+ Return a string in the following format (without white- spaces):
+ <student A>-<average mark>; <student B>- Kaverage mark>;
+ Take the following into account:
+ Any kind of separator can be used in the string. The mark is an integer from 1-10. If the mark is out of that range, discard it, and don't use it in the average calculation.
+
+ Don't forget to think through any corner cases.
+ Examples:
+ Input: [Madeline Choi 5 5 6, Ethan Wyatt 8 9 10] Output: Madeline Choi-5; Ethan Wyatt-9;
+ Input: [Denver Wagner 6 7 8, Ernie Gibbs 11 2] Output: Denver Wagner-7; Ernie Gibbs-2;
+ Input: [Kim Ramos: 4 5 6, Giovanni Landry 10]
+ 10
+ Output: Kim Ramos-5; Giovanni Landry-10;
+ Input: [Henrietta Moon:5;5;8, Edwin Bonilla 100;]
+ Output: Henrietta Moon-6; Edwin Bonilla-0;
+ For this test you're using Java OpenJDK 12
+ Feel free to add comments in your code explaining your solution.
+ n
+ n
+ */
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        AverageMarks am = new AverageMarks();
+        String [] stuarray = new String []{"Madeline Choi 5 5 6","Ethan Wyatt 8 9 10"};
+        am.getStudentAverage(stuarray);
     }
 }
